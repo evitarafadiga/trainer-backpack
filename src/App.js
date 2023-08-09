54b56522ea24945690ab34b74c259abc0a44d781
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2023-08-09 16:56:08 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TrainerCard from "./components/TrainerCard"
+
+export default function App() {
+  return (
+    <div>
+      <div className="flex py-10 justify-center items-center space-x-4 text-base">
+        <TrainerCard />
+      </div>
+    </div>
+
+  )
+}
---
+++ src/components/TrainerCard.js
@@ -0,0 +1,29 @@
+
+const TrainerCard = ({ playTime, money, playerName, cardColor }) => {
+    return (
+        <div className="rounded-lg bg-slate-300 w-[352px] h-[201px]">
+
+            <div className="pb-[149px] pt-[20px]">
+                <div className="bg-gray-700 w-[352px] h-[32px]">
+                    <div className="pb-[112px] pt-[-16px] pl-[14px]">
+                        <div className="rounded-full bg-white w-[90px] h-[90px]">                        
+                        <div className="pb-[154px] pt-[5px] pl-[105px]">
+                        <div className="rounded-lg rounded-e-none bg-slate-300 w-[233px] h-[22px]">
+                            <div className="font-mono pl-2">
+                                {playerName}
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+export default TrainerCard;
